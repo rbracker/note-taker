fa2ba441a63b64a4e7a53cb3ecbbb85c55070325
Ranee Bracker <rbracker@asu.edu>
2024-10-03 09:52:20 -0700

    working on linking errors between notes and index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -29,7 +29,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
 
 app.listen(PORT, () => 
 {
